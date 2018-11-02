a31ddec9f7db445ba4321c0ca535cac137e4b446
Ralph Perdomo <ralph@prdmo.com>
2018-11-02 11:12:19 -0700

    add pizza pricing method to pizzaria object

--- js/script.js
+++ js/script.js
@@ -11,27 +11,32 @@ Pizzaria.prototype.newPie = function(size) {
   this.orderUp.push(new Pizza(this.pizzasMade++,size));
 }
 
+Pizzaria.prototype.calcPrice = function(pizza) {
+  var basePrice = this.pizzaSizesPrice[pizza.size];
+  var addedToppings = 0;
+
+  pizza.toppings.forEach((function(topping) {
+    addedToppings += this.pizzaToppingsPrice[topping];
+  }).bind(this));
+
+  pizza.price = basePrice + addedToppings;
+}
+
 function Pizza(id,size) {
   this.size = size;
   this.pizzaId = id;
 
   this.toppings = [];
+  this.price = 0;
 }
 
 Pizza.prototype.addToppings = function(topping) {
-  this.toppings.push(topping)
+  this.toppings.push(topping);
 }
 
-Pizza.prototype.calcPrice = function() {
-
-}
-
-Pizza.prototype.display = function() {
-  return this
-}
 
 $(function() {
-  var pizzaria1 = new Pizzaria()
+  var pizzaria1 = new Pizzaria();
 
   // renders pizza sizes and prices based on pizzaria constructor
   for (let prop in pizzaria1.pizzaSizesPrice) {
@@ -43,7 +48,6 @@ $(function() {
 
   // renders pizza toppings based on pizzaria constructor
   for (let prop in pizzaria1.pizzaToppingsPrice){
-
     $("#pizzeria-toppings").append(`
       <div class="form-check">
         <input class="form-check-input" type="checkbox" value="${prop}" id="${prop}">
@@ -56,9 +60,9 @@ $(function() {
   $("#buildpie").click(function(){
     var pizzaSize;
 
-    $("input[name=pizza-size]:checked").length ? (pizzaSize = $("input[name=pizza-size]:checked").prop("id")) : (pizzaSize = "small")
+    $("input[name=pizza-size]:checked").length ? (pizzaSize = $("input[name=pizza-size]:checked").prop("id")) : (pizzaSize = "small");
 
-    pizzaria1.newPie(pizzaSize)
+    pizzaria1.newPie(pizzaSize);
 
     var currentPizza = pizzaria1.orderUp[pizzaria1.orderUp.length-1];
 
@@ -67,11 +71,13 @@ $(function() {
       currentPizza.addToppings(top.toString());
     })
 
-    console.log(currentPizza)
+    pizzaria1.calcPrice(currentPizza);
+
+    var pizzaSummary = `<p>One <span style="text-decoration: underline;">${currentPizza.size}</span> pizza with:</p> <ul><li>${currentPizza.toppings.join("</li>\n<li>")}</ul><p>Amount due: $${currentPizza.price}</p>`;
 
     // display pizza details
     $("#order-details").show();
-    $("#pizza-details").text(currentPizza.size +" pizza \n with: " +currentPizza.toppings.join(", "))
+    $("#pizza-details").html(pizzaSummary);
 
 
   })
