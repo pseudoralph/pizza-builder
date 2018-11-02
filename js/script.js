313a9b285cb7dc84611f5337f3ab0cac928db1d6
Ralph Perdomo <ralph@prdmo.com>
2018-11-02 10:10:28 -0700

    render dynamic pizza sizes set by pizzaria constructor

--- js/script.js
+++ js/script.js
@@ -1,46 +1,84 @@
 
 function Pizzaria() {
   this.pizzasMade = 0;
-  this.pizzaSizesPrice = {small: 10, medium: 15, large: 20}
-  this.pizzaToppingsPrice = {cheese: 1, pepperoni: 1, mushrooms: 1}
+  this.pizzaSizesPrice = {small: 10, medium: 15, large: 20};
+  this.pizzaToppingsPrice = {cheese: 1, pepperoni: 1, mushrooms: 1};
 
-  this.orderUp = []
+  this.orderUp = [];
 }
 
 Pizzaria.prototype.newPie = function(size) {
-  this.pizzasMade++
-
-  this.orderUp.push(new Pizza(this.pizzasMade,size))
+  // this.pizzasMade++;
+  this.orderUp.push(new Pizza(this.pizzasMade++,size));
   // this.currentOrder = new Pizza(this.pizzasMade,size);
 
 
 }
 
 function Pizza(id,size) {
-  this.size = size,
-  this.pizzaId = id++
+  this.size = size;
+  this.pizzaId = id;
+
+  this.toppings = [];
 }
 
-Pizza.prototype.build = function(toppings) {
-  this.toppings = toppings
+Pizza.prototype.addToppings = function(topping) {
+  this.toppings.push(topping)
 }
+
+
 
 Pizza.prototype.display = function() {
-  console.log(this.size, this.id)
+  return this
 }
 
-
-
 $(function() {
+  var pizzaria1 = new Pizzaria()
+
+  // uiSizes();
+
+  console.log(pizzaria1.pizzaToppingsPrice)
+  console.log(pizzaria1.pizzaSizesPrice)
+
+  for (let prop in pizzaria1.pizzaSizesPrice) {
+    $("#pizzeria-sizes").append(`
+      <label class="btn btn-secondary">
+        <input type="radio" name="pizza-size" id="${prop}" autocomplete="off">${prop}
+      </label>`);
+    }
 
 
-  var ralphspizzas = new Pizzaria()
+
+  // pizzaria1.pizzaSizesPrice
+  //
+  // uiSizes()
 
 
-  $("#newpie").click(function(){
-    console.log('new pie click')
-    ralphspizzas.newPie('small')
-    console.log(ralphspizzas)
+  $("#buildpie").click(function(){
+    var pizzaSize;
+
+    $("input[name=pizza-size]:checked").length ? (pizzaSize = $("input[name=pizza-size]:checked").prop("id")) : (pizzaSize = "small")
+
+    pizzaria1.newPie(pizzaSize)
+
+    var currentPizza = pizzaria1.orderUp[pizzaria1.orderUp.length-1];
+
+    currentPizza.addToppings("cheese")
+    currentPizza.addToppings("pepperoni")
+    currentPizza.addToppings("mushrooms")
+
+    console.log(currentPizza)
+
+
+
+    // var output = pizzaria1.orderUp[pizzaria1.orderUp.length-1].display()
+    // console.log(output.size, output.toppings)
+
+    // console.log(output)
+
+
+
+    $("#pizza-details").text(currentPizza.size +" pizza \n with: " +currentPizza.toppings.join(", "))
 
 
   })
