4d1efc119ed9f7f4616bb011cf368694b34ac69b
Ralph Perdomo <ralph@prdmo.com>
2018-11-03 11:02:52 -0700

    finalize live toppings adder

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 function Pizzeria() {
   this.tracker = 0;
-  this.pizzaSizesPrice = {small: 10, medium: 15, large: 20};
+  this.pizzaSizesPrice = {small: 10, medium: 15, large: 20, giant: 25};
   this.pizzaToppingsPrice = {cheese: 1, pepperoni: 1, ham:1, sausage: 1, mushroom: 1, onion: 1, garlic: 1, green_peppers: 1, pineapple: 1};
 
   this.orderUp = [];
@@ -19,7 +19,12 @@ Pizzeria.prototype.calcPrice = function(pizza) {
     addedToppings += this.pizzaToppingsPrice[topping];
   }).bind(this));
 
-  pizza.price = basePrice + addedToppings;
+  pizza.price = basePrice + addedToppings + (pizza.delivery ? (2) : (0));
+}
+
+Pizzeria.prototype.removePie = function(pieId) {
+  delete this.orderUp[pieId];
+  console.log(this.orderUp)
 }
 
 function Pizza(id,size) {
@@ -32,8 +37,8 @@ function Pizza(id,size) {
   this.delivery = false;
 }
 
-Pizza.prototype.addToppings = function(topping) {
-  this.toppings.push(topping);
+Pizza.prototype.addToppings = function(toppings) {
+  this.toppings = toppings;
 }
 
 // UI functions
@@ -46,9 +51,9 @@ function ui_makeFirstCharUpper(word) {
 }
 
 function ui_orderReset() {
-  $("input").off('change');
+  // $("input").off('change');
 
-  $("input").prop("disabled",false);
+  // $("input").prop("disabled",false);
   $("input").prop("checked",false);
 
   $("#pizza-details").hide();
@@ -75,33 +80,24 @@ function render_pizzaToppings(pizzeria1) {
   return htmlOutput.join("");
 }
 
-function piePreparer(pizzeria1, size, toppings) {
-  var pizzaSize;
-
-  $("input[name=pizza-size]:checked").length ? (pizzaSize = $("input[name=pizza-size]:checked").prop("id")) : (pizzaSize = "small");
-
-  pizzeria1.newPie(pizzaSize);
+function bakePie(pizzeria1, size="small", toppings, deliver) {
+  pizzeria1.newPie(size);
   var currentPizza = pizzeria1.orderUp[pizzeria1.orderUp.length-1];
-
-  $("input[type=checkbox]:checked").each(function(){
-    var top = $(this).val();
-    currentPizza.addToppings(top.toString());
-  })
-
+  currentPizza.addToppings(toppings);
+  currentPizza.delivery = deliver;
   pizzeria1.calcPrice(currentPizza);
-
-  var pizzaSummary = `<p>One <span style="text-decoration: underline;">${currentPizza.size}</span> pizza with:</p>
-  <ul>${listBuilder(currentPizza.toppings)}</ul>
-  <p>Amount due: $<span id="is-delivered">${currentPizza.price}</span</p>`;
-
-  return pizzaSummary;
+  // 
+  // var pizzaSummary = `<p>One <span style="text-decoration: underline;">${currentPizza.size}</span> pizza with:</p>
+  // <ul>${listBuilder(currentPizza.toppings)}</ul>
+  // <p>Amount due: $<span id="is-delivered">${currentPizza.price}</span</p>`;
+  //
+  // return pizzaSummary;
 }
 
 function listBuilder(toppings) {
   let htmlOutput = []
 
   toppings.forEach(function(topping) {
-
     htmlOutput.push(`<li>${ui_makeFirstCharUpper(topping)}</li>`)
   })
 
@@ -111,7 +107,7 @@ function listBuilder(toppings) {
 function pieViewer(pizzeria1) {
   let currentPizza = pizzeria1.orderUp[pizzeria1.orderUp.length-1]
 
-  let htmlOutput = `<div class="card">
+  let htmlOutput = `<div class="card" id="pizza-card-${currentPizza.pizzaId}">
       <div class="card-header" id="pizzaHeading${currentPizza.pizzaId}">
         <h5 class="mb-0">
           <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapsePizza${currentPizza.pizzaId}" aria-expanded="false" aria-controls="collapsePizza${currentPizza.pizzaId}">🍕 ${currentPizza.size} pizza<span class="float-right">$${currentPizza.price}</span></button>
@@ -134,41 +130,41 @@ function pieViewer(pizzeria1) {
 }
 
 $(function() {
+
+  // inits pizzeria instance
   var pizzeria1 = new Pizzeria();
+  // captures UI changes and stores them for pizza making
+  var liveSize;
+  var liveToppings;
+  var liveDelivery;
+
   // builds dynamic UI elemnts
   $("#pizzeria-sizes").append(render_pizzaSizes(pizzeria1));
   $("#pizzeria-toppings").append(render_pizzaToppings(pizzeria1));
 
-  var liveSize;
-  var liveToppings;
-
   $("input").change(function() {
     liveSize = $("input[name=pizza-size]:checked").prop("id")
-    liveToppings =[];
+    liveDelivery = $("input[name=delivery]:checked").val()
+    liveToppings = [];
 
     $("input[name=toppings]:checked").each(function() {
       liveToppings.push(this.id)
     });
 
+    var htmlOutput = `<p>One ${liveSize}-sized pizza with: </p><ul>${listBuilder(liveToppings)}</ul>`
+    $("#pizza-summary").html(htmlOutput)
+
   });
 
   $("#review").click(function(){
-    console.log(liveToppings)
-    $("#pizza-summary").html(piePreparer(pizzeria1));
-    $("#pizza-details").show();
-
+    $("#pizza-details").toggle();
     console.log(pizzeria1)
-    $("input#delivery").change(function() {
-      $("input#delivery").prop("checked") ? ($("#is-delivered").text('1111')) : ($("#is-delivered").text('666'))
-
-    })
-
-
-
   });
 
   $("#bake").click(function() {
+    $("#pizza-summary").html(bakePie(pizzeria1, liveSize,liveToppings, liveDelivery));
     $("#all-pizza-orders").append(pieViewer(pizzeria1));
+
     ui_orderReset();
   });
 
@@ -177,10 +173,11 @@ $(function() {
   });
 
   $("#all-pizza-orders").on('click','i', function() {
-    const regex = /\d/g;
-    // console.log()
+    let pieToRemove = this.id.match(/\d+$/)[0]
 
-    console.log(this.id.match(/\d+$/)[0])
+    pizzeria1.removePie(pieToRemove);
+    $("#pizza-card-"+pieToRemove).remove()
+
   })
 
 
